4c05598c823ced6e7db34086c91fe0078d4c5475
imin314 <imin314@gmail.com>
2022-03-03 15:10:57 +0700

    Update json rpc urls

--- src/constants.ts
+++ src/constants.ts
@@ -2,12 +2,12 @@ import { NetworkID } from './types';
 import { percentileToBlocks } from './utils/percentileToBlocks';
 
 export const ETHEREUM_JSON_RPC_URLS: Record<NetworkID, string> = {
-  1: 'https://mainnet.infura.io/v3/eea14fc5eadb494abda29dfd5c89201e',
-  4: 'https://rinkeby.infura.io/v3/eea14fc5eadb494abda29dfd5c89201e',
-  56: 'https://bsc-dataseed.binance.org/',
-  97: 'https://data-seed-prebsc-1-s1.binance.org:8545',
-  42161: 'https://arb1.arbitrum.io/rpc',
-  421611: 'https://rinkeby.arbitrum.io/rpc',
+  1: 'https://eth-mainnet.alchemyapi.io/v2/Rz8ueSTVkKGmbcp8JX-Aj8Tx3mTYZSwI',
+  4: 'https://eth-rinkeby.alchemyapi.io/v2/GxeYT3C6LuVsPiTlNj21p6QuJOp3W1A5',
+  56: 'https://bsc.getblock.io/mainnet/?api_key=a6b9f45b-2135-47a6-85ab-7926c15329f4',
+  97: 'https://bsc.getblock.io/testnet/?api_key=a6b9f45b-2135-47a6-85ab-7926c15329f4',
+  42161: 'https://arb-mainnet.g.alchemy.com/v2/Q3oYBFgtujCfn8XH80oYAsIk6JvVOg24',
+  421611: 'https://arb-rinkeby.g.alchemy.com/v2/WAq8j4jLlQFNSfZf43ZVj7OtYcqgkK59',
 };
 
 export const HISTORY_BLOCK_COUNT = 200;
